a0052e80ed776b9f4a30ad219b261e17f47952e8
Garnet 420 <garnetfourtwenty@gmail.com>
2012-12-25 00:15:07 -0500

    Moved stuff into ataca directory Started support for editor view

---
+++ ataca/server/publish.js
@@ -0,0 +1,28 @@
+Puzzles = new Meteor.Collection("puzzles");
+
+Meteor.startup(function () {
+    // code to run on server at startup
+
+
+
+    // remove this line when there are actual puzzles in the database
+    Puzzles.remove({});
+
+    if (Puzzles.find().count() === 0) {
+	var data = [
+	    {name: "Puzzle 1",
+	     contents: "Placeholder for Puzzle 1"
+	    },
+	    {name: "Puzzle 2",
+	     contents: "Placeholder for Puzzle 2"
+	    },
+	    {name: "Puzzle 3",
+	     contents: "Placeholder for Puzzle 3"
+	    }
+	]
+
+	for (var i = 0; i < data.length; i++) {
+	    var puzzle_id = Puzzles.insert({name: data[i].name, contents: data[i].contents});
+	}
+    }
+  })
