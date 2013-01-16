1569e8b750d672d9ee7b5519ff2c26e103f7e0c1
Garnet 420 <garnetfourtwenty@gmail.com>
2013-01-16 01:27:25 -0500

    Moved ok cancel events to lib events.js

--- ataca/client/overview.js
+++ ataca/client/overview.js
@@ -55,30 +55,6 @@ Template.puzzles.name_class = function(){
     return this.name ? '' : 'empty';
 };
 
-var okCancelEvents = function (selector, callbacks) {
-    var ok = callbacks.ok || function () {};
-    var cancel = callbacks.cancel || function () {};
-    
-    var events = {};
-    events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
-	function (evt) {
-	    if (evt.type === "keydown" && evt.which === 27) {
-		// escape = cancel
-		cancel.call(this, evt);
-		
-	    } else if (evt.type === "keyup" && evt.which === 13 ||
-		       evt.type === "focusout") {
-		// blur/return/enter = ok/submit if non-empty
-		var value = String(evt.target.value || "");
-		if (value)
-		    ok.call(this, value, evt);
-		else
-		    cancel.call(this, evt);
-	    }
-	};
-    return events;
-};
-
 Template.puzzles.events(okCancelEvents(
     '#new-puzzle',
     {
